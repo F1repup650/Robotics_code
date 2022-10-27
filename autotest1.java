9aafc7dbc12c329073af31ac9b6a318adaeaac5a
Alexander Maples <74429872+Alexander-Maples@users.noreply.github.com>
2022-10-27 09:24:06 -0500

    Update autotest1.java
    Make mecanumcontrol extend normalcontrol.

--- autotest1.java
+++ autotest1.java
@@ -90,8 +90,32 @@ public class autotest1 extends LinearOpMode
             tRight.setDirection(DcMotor.Direction.FORWARD);
             bLeft.setDirection(DcMotor.Direction.REVERSE);
             bRight.setDirection(DcMotor.Direction.FORWARD);
+            tLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            tRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         }
-
+        public void target(int tLTarget, int tRTarget, int bLTarget, int bRTarget)
+        {
+            tLeft.setTargetPosition(tLTarget);
+            tRight.setTargetPosition(tRTarget);
+            bLeft.setTargetPosition(bLTarget);
+            bRight.setTargetPosition(bRTarget);
+        }
+        public void enableEncoders()
+        {
+            tLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            tRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        public void disableEncoders()
+        {
+            tLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            tRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
         public void forward(double SPD)
         {
             tLeft.setPower(SPD);
@@ -99,7 +123,6 @@ public class autotest1 extends LinearOpMode
             bLeft.setPower(SPD);
             bRight.setPower(SPD);
         }
-
         public void reverse(double SPD)
         {
             tLeft.setPower(-SPD);
@@ -107,7 +130,6 @@ public class autotest1 extends LinearOpMode
             tRight.setPower(-SPD);
             bRight.setPower(-SPD);
         }
-
         public void turnLeft(double SPD)
         {
             tLeft.setPower(-SPD);
@@ -115,7 +137,6 @@ public class autotest1 extends LinearOpMode
             bLeft.setPower(-SPD);
             bRight.setPower(SPD);
         }
-
         public void turnRight(double SPD)
         {
             tLeft.setPower(SPD);
@@ -130,42 +151,7 @@ public class autotest1 extends LinearOpMode
             bRight.setPower(0);
         }
     }
-    public class mecanumcontrol {
-        public void init()
-        {
-            tLeft.setDirection(DcMotor.Direction.REVERSE);
-            tRight.setDirection(DcMotor.Direction.FORWARD);
-            bLeft.setDirection(DcMotor.Direction.REVERSE);
-            bRight.setDirection(DcMotor.Direction.FORWARD);
-        }
-        public void forward(double SPD)
-        {
-            tLeft.setPower(SPD);
-            tRight.setPower(SPD);
-            bLeft.setPower(SPD);
-            bRight.setPower(SPD);
-        }
-        public void reverse(double SPD)
-        {
-            bLeft.setPower(-SPD);
-            bRight.setPower(-SPD);
-            tLeft.setPower(-SPD);
-            tRight.setPower(-SPD);
-        }
-        public void turnLeft(double SPD)
-        {
-            tLeft.setPower(-SPD);
-            bLeft.setPower(-SPD);
-            tRight.setPower(SPD);
-            bRight.setPower(SPD);
-        }
-        public void turnRight(double SPD)
-        {
-            tRight.setPower(-SPD);
-            bRight.setPower(-SPD);
-            tLeft.setPower(SPD);
-            bLeft.setPower(SPD);
-        }
+    public class mecanumcontrol extends normalcontrol {
         public void strafeLeft(double SPD)
         {
             tLeft.setPower(-SPD);
@@ -179,13 +165,6 @@ public class autotest1 extends LinearOpMode
             bRight.setPower(SPD);
             tLeft.setPower(SPD);
             bLeft.setPower(-SPD);
-        }
-        public void stop()
-        {
-            tLeft.setPower(0);
-            tRight.setPower(0);
-            bLeft.setPower(0);
-            bRight.setPower(0);
         }
     }
 }
