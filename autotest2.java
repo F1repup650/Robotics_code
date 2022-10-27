5f9904d23f2e41215705dae88b5fe53c6b4f33f3
Alexander Maples <74429872+Alexander-Maples@users.noreply.github.com>
2022-10-27 09:33:34 -0500

    Update autotest2.java
    Change group to "Test Linear Opmode"

--- autotest2.java
+++ autotest2.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="Test Autonomous OpMode 2", group="Linear Opmode")
+@Autonomous(name="Test Autonomous OpMode 2", group="Test Linear Opmode")
 //@Disabled
 public class autotest2 extends LinearOpMode
 {
