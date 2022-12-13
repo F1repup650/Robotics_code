53d3dbd9efc53d5252cd2c7ecac2cef03751f637
Alexander Maples <74429872+Alexander-Maples@users.noreply.github.com>
2022-12-13 15:42:20 -0600

    Update testclawservo.java
    Minimization and class usage

--- testclawservo.java
+++ testclawservo.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name="Claw Servos", group="Tests")
-public class TestClawServo extends LinearOpMode {
+public class testclawservo extends LinearOpMode {
     private boolean buttonPreviousState;
     public boolean buttonClick (boolean button) {
         boolean returnVal;
@@ -18,52 +15,26 @@ public class TestClawServo extends LinearOpMode {
     }
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        servoControl servo = new servoControl();
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("top_left");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("back_left");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("top_right");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("back_right");
-        DcMotor motorArm = hardwareMap.dcMotor.get("arm_motor");
-        Servo claw = hardwareMap.servo.get("Claw");
-        Servo wrist = hardwareMap.servo.get("Wrist");
+        servocontrol servo = new servocontrol();
+        CRServo claw = hardwareMap.crservo.get("Claw");
+        CRServo wrist = hardwareMap.crservo.get("Wrist");
         double clawPower;
         double wristPower;
-        // Reverse the right side motors
-        // Reverse left motors if you are using NeveRests
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double lx = gamepad1.left_stick_x; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
-            double ax = gamepad2.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            /*double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = ((y + x + rx) / denominator);
-            double backLeftPower = ((y - x + rx) / denominator);
-            double frontRightPower = ((y - x - rx) / denominator);
-            double backRightPower = ((y + x - rx) / denominator);
-            double armPower = ax;
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
-            motorArm.setPower(ax);
-             */
-            clawPower = servo.stickControl( x );
-            wristPower = servo.stickControl( rx );
-            claw.setPosition( clawPower );
-            wrist.setPosition( wristPower );
+            clawPower = servo.stickControl(lx);
+            wristPower = servo.stickControl(rx);
+            claw.setPower( clawPower );
+            wrist.setPower( wristPower );
+            telemetry.addData("Servos","Claw servo power: %4.2f", clawPower);
+            telemetry.addData("Servos","Wrist servo power: %4.2f", wristPower);
+            telemetry.update();
         }
     }
 }
