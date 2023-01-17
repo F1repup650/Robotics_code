9c8533724c5497209b0ae037c5e58a641c994f17
Alexander-Maples <mapleale@studentmail.conroeisd.net>
2023-01-17 16:56:26 -0600

    Show carson how to commit changes

--- mecanumteleop.java
+++ mecanumteleop.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServo; // Carson, why aren't we using the Continuous Rotation Servos?
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Mecanum: Test Linear OpMode", group="Linear Opmode")
+@TeleOp(name="Mecanum: Test Linear OpMode", group="Production")
 public class MecanumTeleOp extends LinearOpMode {
     private boolean buttonPreviousState;
     public boolean buttonClick (boolean button) {
@@ -24,12 +25,7 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("top_right");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("back_right");
         DcMotor motorExtension = hardwareMap.dcMotor.get("motor_up");
-        CRServo claw = hardwareMap.crservo.get("Claw");
-        CRServo wrist = hardwareMap.crservo.get("Wrist");
-        double clawPower;
-        double motorEx;
-        double wristPower;
-
+        Servo motorGrab = hardwareMap.servo.get("servo");
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
         motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -43,41 +39,37 @@ public class MecanumTeleOp extends LinearOpMode {
             double y = gamepad1.left_stick_y * -1; // Remember, this is reversed!
             double rx = gamepad1.left_stick_x * -1; // Counteract imperfect strafing
             double x = gamepad1.right_stick_x * -1;
-            double rxs = gamepad2.right_trigger;// motor extension
-            double rxa = gamepad2.left_trigger * -1;// motor extension
-            double wcx = gamepad2.right_stick_y;// wrist rotation
-            boolean ba = gamepad2.a;
-            boolean bb = gamepad2.b;
+            boolean rb = gamepad1.right_bumper;
+            boolean lb = gamepad1.left_bumper;
+            double rt = gamepad1.right_trigger;
+            double lt = gamepad1.left_trigger;
 
+            int rbp = 2;
+            double lbp = 0.25;
+            if(buttonClick(rb)) {
+                rbp -= 1.5;
+            }
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 2);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), rbp);
             double frontLeftPower = ((y + x + rx) / denominator);
             double backLeftPower = ((y - x + rx) / denominator);
             double frontRightPower = ((y - x - rx) / denominator);
             double backRightPower = ((y + x - rx) / denominator);
-            double b = .5;
-            if(ba) {
-                b = 1;
+            double tt = (lt * -1) + rt;
+
+            if(buttonClick(lb)) {
+                lbp -= 0.2;
             }
-            if(bb) {
-                b = 0;
-            }
+            motorGrab.setPosition(lbp);
+
 
             motorFrontLeft.setPower(frontLeftPower);
             motorBackLeft.setPower(backLeftPower);
             motorFrontRight.setPower(frontRightPower);
             motorBackRight.setPower(backRightPower);
-            clawPower = b;
-            wristPower = wcx;
-            motorEx = (rxa + rxs) * .75;
-            claw.setPower( clawPower );
-            wrist.setPower( wristPower );
-            motorExtension.setPower(motorEx);
-            telemetry.addData("Servos","Claw servo position: %4.2f", clawPower);
-            telemetry.addData("Servos","Wrist servo position: %4.2f", wristPower);
-            telemetry.update();
+            motorExtension.setPower(tt);
         }
     }
 }
