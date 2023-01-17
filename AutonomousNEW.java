e85d748ff04b2b119d8bc4fdc766dcf1d53ccb6e
Alexander-Maples <mapleale@studentmail.conroeisd.net>
2023-01-17 16:29:09 -0600

    INIT

---
+++ AutonomousNEW.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="Autonomous OpMode (Park and Place)")
+public class AutonomousNEW extends LinearOpMode {
+    ColorSensor color;
+    DcMotor tLeft;
+    DcMotor tRight;
+    DcMotor bLeft;
+    DcMotor bRight;
+    Servo motorGrab;
+    String colorDetected = "";
+    double FWDSPD = 0.5;
+    double TRNSPD = 0.25;
+    double STRSPD = 0.3;
+    double ENCSPD = 0.3;
+
+    @Override
+    public void runOpMode() {
+        // CARSON THERE IS A CLASS FOR AUTONOMOUS MOTOR CONTROL!!! IN FACT, THERE'S TWO OF THEM!
+        mecanumcontrol motor = new mecanumcontrol();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        tLeft = hardwareMap.dcMotor.get("top_left");
+        tRight = hardwareMap.dcMotor.get("top_right");
+        bLeft = hardwareMap.dcMotor.get("back_left");
+        bRight = hardwareMap.dcMotor.get("back_right");
+        color = hardwareMap.get(ColorSensor.class, "Color1");
+        DcMotor motorExtension = hardwareMap.dcMotor.get("motor_up");
+        motorGrab = hardwareMap.servo.get("servo");
+        motor.init();
+        color = hardwareMap.get(ColorSensor.class, "Color1");
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+        waitForStart();
+        // Go forward for 2.5 seconds
+        motor.forward(FWDSPD);
+        motorExtension.setPower(1.0);
+        sleep(500);
+        motorExtension.setPower(0.0);
+        sleep(1000);
+        motor.stop();
+        sleep(1000);
+        while(opModeIsActive()) {
+            // This entire function does nothing, as "colorDetected" is set to "" at the top of the code
+            if (color.red() > color.blue() && color.red() > color.green())// If RED is greater than all other colors
+            {
+                telemetry.addData("Red: ", color.red());
+                telemetry.update();
+                colorDetected = "red";
+
+
+            }
+            else if (color.green() > color.blue() && color.green() > color.red())// If GREEN is greater than all other colors
+            {
+                telemetry.addData("green", color.green());
+                telemetry.update();
+                colorDetected = "green";
+
+            }
+            else if (color.blue() > color.red() && color.blue() > color.green())// If BLUE is greater than all other colors
+            {
+                telemetry.addData("Blue:", color.blue());
+                telemetry.update();
+                colorDetected = "blue";
+
+            }
+            motorExtension.setPower(1.0);
+            sleep(2500);
+            motorExtension.setPower(0.0);
+            motor.turnRight(STRSPD);
+            sleep(200);
+            motor.stop();
+            break;
+        }
+    }
+}
---
+++ normalcontrol.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class normalcontrol {
+    DcMotor tLeft;
+    DcMotor tRight;
+    DcMotor bLeft;
+    DcMotor bRight;
+    DcMotor mArm;
+    public void init()
+    {
+        tLeft.setDirection(DcMotor.Direction.REVERSE);
+        tRight.setDirection(DcMotor.Direction.FORWARD);
+        bLeft.setDirection(DcMotor.Direction.REVERSE);
+        bRight.setDirection(DcMotor.Direction.FORWARD);
+        mArm.setDirection(DcMotor.Direction.FORWARD);
+        tLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        tRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void target(int tLTarget, int tRTarget, int bLTarget, int bRTarget)
+    {
+        tLeft.setTargetPosition(tLTarget);
+        tRight.setTargetPosition(tRTarget);
+        bLeft.setTargetPosition(bLTarget);
+        bRight.setTargetPosition(bRTarget);
+    }
+    public void enableEncoders()
+    {
+        tLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        tRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void disableEncoders()
+    {
+        tLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        tRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public void forward(double SPD)
+    {
+        tLeft.setPower(SPD);
+        tRight.setPower(SPD);
+        bLeft.setPower(SPD);
+        bRight.setPower(SPD);
+
+    }
+    public void reverse(double SPD)
+    {
+        tLeft.setPower(-SPD);
+        bLeft.setPower(-SPD);
+        tRight.setPower(-SPD);
+        bRight.setPower(-SPD);
+    }
+    public void turnLeft(double SPD)
+    {
+        tLeft.setPower(-SPD);
+        tRight.setPower(SPD);
+        bLeft.setPower(-SPD);
+        bRight.setPower(SPD);
+    }
+    public void turnRight(double SPD)
+    {
+        tLeft.setPower(SPD);
+        tRight.setPower(-SPD);
+        bLeft.setPower(SPD);
+        bRight.setPower(-SPD);
+    }
+    public void stop() {
+        tLeft.setPower(0);
+        tRight.setPower(0);
+        bLeft.setPower(0);
+        bRight.setPower(0);
+    }
+    public void strafeLeft(double SPD)
+    {
+        tLeft.setPower(SPD);
+        bLeft.setPower(-SPD);
+        tRight.setPower(-SPD);
+        bRight.setPower(SPD);
+    }
+    public void strafeRight(double SPD)
+    {
+        tRight.setPower(SPD);
+        bRight.setPower(-SPD);
+        tLeft.setPower(-SPD);
+        bLeft.setPower(SPD);
+    }
+}
